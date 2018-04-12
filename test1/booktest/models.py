c23eec229f9778162a9f047b0d3b76e70e70f374
linian <1109025424@qq.com>
2018-04-12 17:31:55 +0800

    v1

---
+++ test1/booktest/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class BookInfo(models.Model):
+    objects = models.Manager()
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField()
+    def __str__(self):
+        return self.btitle
+
+class HeroInfo(models.Model):
+    objects = models.Manager()
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=100)
+    hBook = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.hname
